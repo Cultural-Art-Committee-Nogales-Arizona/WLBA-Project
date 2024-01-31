a91954f8a886a3a44e59f3f60ad15d5a9a8231b9
NikoDrossos <ndross427@West-MEC.org>
2024-01-31 14:29:08 -0700

    Added festival API

--- app/api/events/festivals/route.js
+++ app/api/events/festivals/route.js
@@ -1,15 +1,52 @@
-export const GET = () => {
-    //get either all festivals for display in the dashboard or calendar, or get closest festival
+import { NextResponse } from "next/server"
+import Festival from "@/models/events/Festivals"
+
+//get either all festivals for display in the dashboard or calendar, or get closest festival
+export const GET = async () => {
+  try {
+    const result = await Festival.find()
+
+    return NextResponse.json({
+      success: true,
+      message: `Successfully fetched all festivals`,
+      data: result
+    })
+  } catch (err) {
+    return NextResponse.json({
+      success: false,
+      message: `An error occurred fetching Festivals`,
+      errorMessage: err.message,
+      error: err
+    })
+  }
 }
 
-export const POST = () => {
-    //add a festival
+//add a festival
+export async function POST(request) {
+  try {
+    const requestBody = await request.json()
+
+    const result = await Festival.create(requestBody)
+
+    return NextResponse.json({
+      success: true,
+      message: `Successfully created festival ${requestBody.title}`,
+      data: result
+    })
+  } catch (err) {
+    return NextResponse.json({
+      success: false,
+      message: `An error occurred fetching Festivals`,
+      errorMessage: err.message,
+      error: err
+    })
+  }
 }
 
+//edit festival
 export const PUT = () => {
-    //edit festival
 }
 
+//delete a festival
 export const DELETE = () => {
-    //delete a festival
 }
