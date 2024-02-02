a108566e9dda4537fafc093b7fca24b5bdde8845
AvarageCodeEnjoyer <111468739+AvarageCodeEnjoyer@users.noreply.github.com>
2024-02-01 17:33:45 -0700

    Removed fontawsome

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -32,7 +32,7 @@ const NavBar = () => {
       <Navbar color="light" light expand="md">
         <a href="/">
           <Image
-            src={"/images/Logo.png"}
+            src={"/Logo.png"}
             alt={"C.A.C.N.A Logo"}
             width={75}
             height={75}
@@ -69,7 +69,7 @@ const NavBar = () => {
                       <span className={styles.link_animation}>Volunteer</span>
                     </PageLink>
                   </NavItem>
-                  {/* Add this as a donate button later */}
+                  {/*  Add this as a donate button later */}
                   {/* <NavItem>
                     <PageLink href="/stripe" className="nav-link" testId="navbar-stripe">
                       Stripe Page
