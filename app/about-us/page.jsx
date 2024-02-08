d22e1bbafc5abd8f8967f0cff5c5107d68eb070c
Nathen <nhadden014@west-mec.org>
2024-02-08 13:33:38 -0700

    Merge branch 'main' of https://github.com/Cultural-Art-Committee-Nogales-Arizona/WLBA-Project

--- app/about-us/page.jsx
+++ app/about-us/page.jsx
@@ -77,12 +77,14 @@ export default function aboutUs() {
 
         <div className={styles.container2}>
           <div className={styles.child1}>
-            <Image
-              width={300}
-              height={150}
-              src={AboutUsImagetwo}
-              alt={"text"}
-            />
+            <div>
+              <Image
+                width={330}
+                height={248}
+                src={AboutUsImagetwo}
+                alt={"text"}
+              />
+            </div>
 
             <p>
               Esther Melendez-Lopez, seeking re-election to the Nogales City
@@ -100,7 +102,26 @@ export default function aboutUs() {
             </p>
           </div>
 
-          <div className={styles.child2}></div>
+          <div className={styles.child2}>
+            <p>
+              Esther Melendez-Lopez, seeking re-election to the Nogales City Council, emphasizes her commitment to cultural events,
+              tourism, affordable housing, and public safety. As a member of the Cultural Arts Committee, she has been instrumental
+              in organizing events to enrich the community. Melendez-Lopez supports the IME Becas scholarship program and defends her
+              focus on cultural events, asserting her active engagement in council matters. She advocates for affordable housing,
+              backs the city's annexation plan for growth, and prioritizes public safety by allocating COVID relief funds to pay down
+              the pension debt. Overall, she presents herself as a dedicated community leader with a comprehensive approach to
+              addressing Nogales' diverse needs.
+            </p>
+
+            <div>
+              <Image
+                width={330}
+                height={248}
+                src={AboutUsImagetwo}
+                alt={"text"}
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
