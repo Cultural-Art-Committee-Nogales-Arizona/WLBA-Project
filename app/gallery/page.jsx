2c864a30d2a1d6eeb3abd9f5886307fdb6f529f0
Jaden TG <trujequeGoh12@gmail.com>
2024-02-09 12:30:05 -0700

    .

--- app/gallery/page.jsx
+++ app/gallery/page.jsx
@@ -3,7 +3,6 @@
 import React from 'react';
 
 import Hero from '../../components/Hero';
-import Content from '../../components/Content';
 
 export default function Gallery() {
   return (
@@ -11,7 +10,6 @@ export default function Gallery() {
       <h1>This is the Gallery page</h1>
       <Hero />
       <hr />
-      <Content />
     </>
   );
 }
