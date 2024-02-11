6a3d1e1e0de687794564e1020bfa63dfc0ebb186
AvarageCodeEnjoyer <111468739+AvarageCodeEnjoyer@users.noreply.github.com>
2024-02-11 15:41:56 -0700

    Removed unnecessary packages

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -14,8 +14,6 @@ import {
   DropdownMenu,
   DropdownItem
 } from 'reactstrap'
-import { useHistory } from 'react-router-dom';
-import { useAuth0 } from '@auth0/auth0-react';
 import { useUser } from '@auth0/nextjs-auth0/client'
 import WhiteLogo from '@/public/whiteLogo'
 import CustomUserContext from './GlobalUserContext'; 
