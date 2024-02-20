47a20190e8cb41cb2248a602de85d879a78315df
AvarageCodeEnjoyer <111468739+AvarageCodeEnjoyer@users.noreply.github.com>
2024-02-19 22:55:31 -0700

    Added favicons, fixed build

--- utils/ImportGalleryImages.jsx
+++ utils/ImportGalleryImages.jsx
@@ -1,4 +1,4 @@
-import IMG_3888 from '@/public/Images/gallery/IMG_3888.jpg';
+/* import IMG_3888 from '@/public/Images/gallery/IMG_3888.jpg';
 import IMG_3895 from '@/public/Images/gallery/IMG_3895.jpg';
 import IMG_3898 from '@/public/Images/gallery/IMG_3898.jpg';
 import IMG_3899 from '@/public/Images/gallery/IMG_3899.jpg';
@@ -11,3 +11,4 @@ import IMG_3930 from '@/public/Images/gallery/IMG_3930.jpg';
 import IMG_7150 from '@/public/Images/gallery/IMG_7150.jpg';
 import IMG_9000 from '@/public/Images/gallery/IMG_9000.jpg';
 export default [IMG_3888, IMG_3895, IMG_3898, IMG_3899, IMG_3903, IMG_3911, IMG_3914, IMG_3922, IMG_3928, IMG_3930, IMG_7150, IMG_9000];
+ */
