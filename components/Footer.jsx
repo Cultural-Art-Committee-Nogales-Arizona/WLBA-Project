2f710a81c55c9991c285b6a4c35b9d0f1eba97b7
Jaden TG <trujequeGoh12@gmail.com>
2024-02-26 12:07:40 -0700

    Made the same mistake

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -12,11 +12,11 @@ const Footer = () => (
       <PageLink href="/about-us" className={styles.link} testId="navbar-ssr">
         <span>About Us</span>
       </PageLink>
-      <PageLink href="/gallery" className={styles.link} testId="navbar-external">
-        <span>Gallery</span>
+      <PageLink href="https://www.google.com/maps/place/Nogales,+AZ+85621/@31.3643595,-111.0155213,12z/data=!3m1!4b1!4m6!3m5!1s0x86d6a7b602bcccef:0x55dc0a7c4ff9d977!8m2!3d31.3403775!4d-110.9342532!16zL20vMHFxdzk?entry=ttu" className={styles.link} testId="navbar-external">
+        <span>Location</span>
       </PageLink>
-      <PageLink href="/calendar" className={styles.link} testId="navbar-csr">
-        <span>Events</span>
+      <PageLink href="https://buy.stripe.com/eVa6ptaH472hgkE000" className={styles.link}>
+        <span>Donate</span>
       </PageLink>
     </div>
     <div className={styles.navLinks}>
