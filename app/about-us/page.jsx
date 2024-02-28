b1fd9b0d2cde356e1a5b30ed0cdcf302779c9ee4
Jaden TG <trujequeGoh12@gmail.com>
2024-02-28 12:57:53 -0700

    Semi-Finished About Us

--- app/about-us/page.jsx
+++ app/about-us/page.jsx
@@ -26,7 +26,10 @@ export default function aboutUs() {
   console.log(ImportDirectorInformation)
   return (
     <div className={styles.aboutUsContainer} >
-      <Hero params={{ heroImage }} />
+      <Hero 
+        params={{ heroImage }} 
+        className={styles.heroImage}
+      />
       <div className={`${styles.headText} ${artesaniaFont.className}`}>
         <div>
           <h1>About Us</h1>
@@ -48,18 +51,18 @@ export default function aboutUs() {
       </div>
 
       <div className={styles.godfather}>
-        <div className={styles.container2}>
-          <div className={styles.child1}>
-            {/* <div>
-              <Image
+        <div className={styles.container1}>
+          <div className={styles.Imagec1}>
+              {/* <Image
                 width={220}
                 height={290}
                 src={AboutUsImagetwo}
-                className={styles.Imagec1}
+                
                 alt={"text"}
-              />
-            </div> */}
+              /> */}
+            </div>
 
+          <div className={styles.child1}>
             <p>
               Esther Melendez-Lopez, seeking re-election to the Nogales City
               Council, emphasizes her commitment to cultural events, tourism,
@@ -86,37 +89,20 @@ export default function aboutUs() {
               multifaceted contributions and expertise showcase her passion for 
               preserving and promoting cultural arts in the community.
             </p>
+          </div>
 
-            {/* <div>
-              <Image
+          <div className={styles.Imagec2}>
+              {/* <Image
                 width={220}
                 height={290}
                 src={AboutUsImagethree}
-                className={styles.Imagec2}
+                
                 alt={"text"}
-              />
-            </div> */}
-          </div>
+              /> */}
+            </div>
         </div>
       </div>
-      {/* <div className={styles.container}>
-      <Image
-        className={`${styles.bannerImage} ${styles.transparent}`} 
-        src={AboutUsImageone}
-        alt={"Banner"}
-        
-      />
-        <div className={styles.header}>
-          <div className={styles.header2}>
-            <div className={styles.header3}>
-          <div className={styles.auc}>About us</div>
-            Established in 2014 as a non-profit organization, the Cultural Arts Committee of Nogales, Arizona, has been busy creating events that bring the community together. We've hosted all sorts of things, from concerts and mariachi festivals to art workshops for kids and nativity exhibitions. In collaboration with the City of Nogales and the Consulate of Mexico, our programs cover a wide range, including scholarship initiatives, talks for Women's Week, and even a binational marathon. What makes it all happen? Our dedicated team of volunteers. The Board of Directors, made up of community members, including the former Consul General of Mexico, and local business leaders, helps steer the ship. We're all about supporting and celebrating our community.
-          </div>
-          </div>
-        </div>
-      </div>
-      */}
-
+      
       <div className={styles.eventDiv}>
         <h1>Our Events</h1>
         <div className={styles.eventsContainer}>
