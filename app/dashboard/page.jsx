8d1600689058b4ba85d15579bd8da5bf42183632
Jesuslara14 <111470200+Jesuslara14@users.noreply.github.com>
2024-02-29 14:20:17 -0700

    removed one line of code

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -100,7 +100,6 @@ function Profile() {
               <h3>Vendor Information</h3>
               <div>
                 {/* Change this to vendor info */}
-                <p>Current Next Event: {pageData.nextEvent.title || "No upcoming events"}</p>
                 <p>Registered Vendors: {pageData.allEvents.length}</p>
               </div>
               <hr />
